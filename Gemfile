4394f0e22eaf8d4ba96cd07df703329f5931b6e2
minoriakita <minori.akita@gmail.com>
2022-06-08 12:39:57 +0000

    [add] gem install

--- Gemfile
+++ Gemfile
@@ -54,3 +54,10 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem 'devise'
+gem "enum_help"
+gem 'kaminari','~> 1.2.1'
+gem "font-awesome-rails"
+gem 'pry-rails'
+gem 'rails-i18n'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "bf47b757b9a04ee381ec5cca872f2249.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
