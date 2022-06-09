fece7e799eae7c96b10a755900c49a4dbaab8122
minoriakita <minori.akita@gmail.com>
2022-06-09 12:37:09 +0000

    患者新規登録途中

--- app/controllers/admin/patients_controller.rb
+++ app/controllers/admin/patients_controller.rb
@@ -1,26 +1,30 @@
 class Admin::PatientsController < ApplicationController
-  
+
   def new
     @patient = Patient.new
   end
-  
+
   def create
+    patient = Patient.new(patient_params)
+    patient.save
+    redirect_to admin_patient_path(@patient.id)
   end
-  
+
   def index
   end
-  
+
   def show
+    @patient = Patient.find(params[:id])
   end
-  
+
   def edit
   end
-  
+
   def update
   end
-  
+
   private
-  
+
   def patient_params
     params.require(:patient).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :birthday, :gender, :email, :postal_code, :address, :telephone_number)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
     sessions: "admin/sessions"
   }
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :admin do
     resources :appointments
     resources :employees
