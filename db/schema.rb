5473f076d87a4205bea81be83e75cec5ec5fc44c
minoriakita <minori.akita@gmail.com>
2022-06-11 07:13:31 +0000

    [Add] 注意事項カラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_09_032742) do
+ActiveRecord::Schema.define(version: 2022_06_11_070122) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 2022_06_09_032742) do
     t.string "telephone_number"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "patient_text"
   end
 
   create_table "symptoms", force: :cascade do |t|
