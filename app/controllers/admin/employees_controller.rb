808ee1be6bf07c1223e9a78b663e181d65828ef2
minoriakita <minori.akita@gmail.com>
2022-06-14 08:06:34 +0000

    [Update] 各項目追加フォーム

--- app/controllers/admin/employees_controller.rb
+++ app/controllers/admin/employees_controller.rb
@@ -1,4 +1,34 @@
 class Admin::EmployeesController < ApplicationController
-  def new
+  def index
+    @employee = Employee.new
+    @employees = Employee.all
+  end
+  
+  def create
+    @employee = Employee.new(employee_params)
+    @employee.save
+    redirect_to admin_employees_path
+  end
+  
+  def edit
+    @employee = Employee.find(params[:id])
+  end
+
+  def update
+    @employee = Employee.find(params[:id])
+    @employee.update(employee_params)
+    redirect_to admin_employees_path
+  end
+
+ def destroy
+    employee = Employee.find(params[:id])
+    employee.delete
+    redirect_to admin_employees_path
+ end
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(:name)
   end
 end
--- app/controllers/admin/symptoms_controller.rb
+++ app/controllers/admin/symptoms_controller.rb
@@ -1,2 +1,34 @@
 class Admin::SymptomsController < ApplicationController
+  def index
+    @symptom = Symptom.new
+    @symptoms = Symptom.all
+  end
+
+  def create
+    @symptom = Symptom.new(symptom_params)
+    @symptom.save
+    redirect_to admin_symptoms_path
+  end
+
+  def edit
+    @symptom = Symptom.find(params[:id])
+  end
+
+  def update
+    @symptom = Symptom.find(params[:id])
+    @symptom.update(symptom_params)
+    redirect_to admin_symptoms_path
+  end
+
+ def destroy
+    symptom = Symptom.find(params[:id])
+    symptom.delete
+    redirect_to admin_symptoms_path
+ end
+
+  private
+
+  def symptom_params
+    params.require(:symptom).permit(:name)
+  end
 end
