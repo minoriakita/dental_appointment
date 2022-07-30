0fa197b7d4fd46e44a5fe08fcf179a1e97b9a420
minoriakita <minori.akita@gmail.com>
2022-07-30 09:58:42 +0000

    [Fix] emailカラム

--- db/migrate/20220714105434_add_devise_to_patients.rb
+++ db/migrate/20220714105434_add_devise_to_patients.rb
@@ -4,7 +4,7 @@ class AddDeviseToPatients < ActiveRecord::Migration[6.1]
   def self.up
     change_table :patients do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      # t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
