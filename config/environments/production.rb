7938900a81a6575881e539a7449a84e2d7020ea0
minoriakita <minori.akita@gmail.com>
2022-07-31 10:29:44 +0000

    [Fix] メール機能

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "dental_appointment_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = {host: "http://http://35.79.57.232/"}
+  config.action_mailer.default_url_options = {host: "http://http://35.79.57.232"}
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
